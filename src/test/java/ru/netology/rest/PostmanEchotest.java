9cf8fb5388e9e9afb6861aa50839decec26b904e
Sergei Demin <wsch@bk.ru>
2023-03-30 14:22:34 +0200

    first

---
+++ src/test/java/ru/netology/rest/PostmanEchotest.java
@@ -0,0 +1,21 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+
+    @Test
+    void shouldReturnSendData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Data"));
+    }
+}
+
